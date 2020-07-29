eb4b69f744a4edd1de164ef7565486c27a42e461
meprasetyo <me.serj.adam@gmail.com.com>
2020-07-29 08:31:41 +0700

    upload template barugasikola

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function buildItem(id, type, length, src, preview, link, seen, time){
+    return {
+                "id": id,
+                "type": type,
+                "length": length,
+                "src": src,
+                "preview": preview,
+                "link": link,
+                "seen": seen,
+                "time": time
+            };
+}
+
+var initDemo = function(){
+    
+};
+
+initDemo();
